2bf75d223a7abb7ac7b8f09aeb42bc0afd829735
is13mael <ismaelparramoussaif@gmail.com>
2025-10-27 19:15:23 +0100

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        try{
         System.out.print("Por favor, introduzca un número entero: ");
         int numero = Integer.parseInt(System.console().readLine());
 
@@ -9,5 +10,10 @@ public class App {
             factorial *=i;
         }
         System.out.println(numero+"! = "+factorial);
+    }catch(NumberFormatException e){
+        System.out.println("Numero mal introducido.");
+    }catch(Exception e){
+        System.out.println("Ha ocurrido un error inesperado.");
     }
+}
 }
